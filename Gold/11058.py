bd4eb7f2fb74f9c1daaafb2b0117f778c347afdc
frica12 <fricaa@naver.com>
2023-02-06 08:18:03 +0900

    update

--- Gold/11058.py
+++ Gold/11058.py
@@ -9,8 +9,11 @@ dp = [i for i in range(N+1)]
 
 for i in range(3, N-2):
     copy = dp[i]
-    print(i, copy)
     dp[i+3] = max(dp[i]*2, dp[i+3])
     if(i > 4):
         dp[i+3] = max(dp[i]*2, dp[i+2]+copy, dp[i+3])
+        # dp[i+3] => 원래 있던거
+        # dp[i]*2 => dp[i]를 붙여넣기한거
+        # dp[i+2]+copy => 현재상태에서 컨트롤V 누른거
+
 print(dp)
