6134be6c4fd387a98f5f6153ce8f7d1b3b419b69
frica12 <fricaa@naver.com>
2023-03-23 05:46:53 +0900

    20230323

--- Gold/13250copy.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-dp = [0]*1010101
-
-for i in range(N-1, -1, -1):
-    ret = 0
-    for j in range(1, 7): ret += 1/6 * (1 + dp[i+j])
-    dp[i] = ret
-
-print(dp[0])
--- Gold/2240_unsolved.py
+++ Gold/2240_unsolved.py
@@ -5,30 +5,52 @@ import sys
 T, W = map(int, sys.stdin.readline().split())
 
 plum = list(int(sys.stdin.readline()) for _ in range(T))
-tree1 = [0 for _ in range(T)]
-tree2 = [0 for _ in range(T)]
 
-for i in range(T):
+dp = [[[0 for _ in range(W+1)] for _ in range(2)] for _ in range(T+1)]
 
-    if(plum[i] == 1):
-        tree1[i] = tree1[i-1]+1
-        tree2[i] = tree2[i-1]
+sum = 0
+
+# dp[T][2][W]
+
+# dp[T][0][W] => T초에, 0번 나무 밑에 있을 때 받아먹을 수 있는 자두의 최대 개수
+# dp[T][1][W] => T초에, 1번 나무 밑에 있을 때 받아먹을 수 있는 자두의 최대 개수
+
+if(plum[0] == 1):
+    for j in range(W+1):
+        dp[1][0][j] = 1
+
+else:
+    for j in range(W+1):
+        dp[1][1][j] = 1
+        if(j >= 1):
+            dp[1][0][j-1] = dp[1][0][j] + 1
+
+for i in range(2, T+1):
+    if(plum[i-1] == 1):
+        for j in range(W+1):
+            dp[i][0][j] = dp[i-1][0][j] + 1
+            dp[i][1][j] = dp[i-1][1][j]
+            if(j >= 1):
+                if(dp[i][1][j] == dp[i-1][1][j]):
+                    dp[i][1][j-1] = dp[i][1][j] + 1
 
     else:
-        tree2[i] = tree2[i-1]+1
-        tree1[i] = tree1[i-1]
+        for j in range(W+1):
+            dp[i][1][j] = dp[i-1][1][j] + 1
+            dp[i][0][j] = dp[i-1][0][j]
+            if(j >= 1):
+                if(dp[i][0][j] == dp[i-1][0][j]):
+                    dp[i][0][j-1] = dp[i][0][j] + 1
+                
 
-max1 = max(tree1)
-max2 = max(tree2)
+for i in range(T+1):
+    print(dp[i][0])
 
-for i in range(T):
-    tree1[i] = max1 - tree1[i]
-    tree2[i] = max2 - tree2[i]
+print("---------")
 
-print(tree1)
-print(tree2)
+for i in range(T+1):
+    print(dp[i][1])
 
-dp = [0 for _ in range(W)]
+sum = max(max(dp[T][0]), max(dp[T][1]))
 
-for i in range(W):
-    pass
+print(sum)
