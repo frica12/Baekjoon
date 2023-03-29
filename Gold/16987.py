6753c0e7efb1074d27de102e32a4326dd243b6ba
frica12 <fricaa@naver.com>
2023-03-29 12:51:50 +0900

    labtop

--- Gold/16987.py
+++ Gold/16987.py
@@ -14,45 +14,51 @@ maxval = 0
 for i in range(N):
     egg[i] = list(map(int, sys.stdin.readline().split()))
 
-def DFS(k, cnt):
+def DFS(k):
     global maxval
     if(k == N):
-        maxval = max(maxval, cnt)
+        maxval = max(maxval, sum(broken))        
+        
     else:
+        cnt = 0
+        isbroke = 0
+        isbroke2 = 0
         for i in range(N):
-            # egg[k] -> 현재 계란
-            temp = 0
-            isbroke = 0
-            isbroke2 = 0
-
-            if(broken[k] == 1):
-                DFS(k+1, cnt)
-            if(broken[i] == 1):
-                continue
-
+            
             if(k != i):
-                
+                if(broken[k] == 1):
+                    DFS(k+1)
+                    continue
+            
+                if(broken[i] == 1):
+                    cnt += 1
+                    continue
+                    
                 egg[k][0] -= egg[i][1]
                 if(egg[k][0] <= 0):
                     broken[k] = 1
-                    cnt += 1
-                    temp += 1
                     isbroke += 1
 
                 egg[i][0] -= egg[k][1]
                 if(egg[i][0] <= 0):
                     broken[i] = 1
-                    cnt += 1
-                    temp += 1
                     isbroke2 += 1
-
-                DFS(k+1, cnt)
+                    
+                DFS(k+1)
                 
+                
+                egg[k][0] += egg[i][1]
                 if(isbroke == 1):
                     broken[k] = 0
+                    isbroke = 0
+                    
+                egg[i][0] += egg[k][1]
                 if(isbroke2 == 1):
                     broken[i] = 0
-                cnt -= temp
+                    isbroke2 = 0
+                    
+        if(cnt == N-1):
+            DFS(k+1)
 
-DFS(0, 0)
+DFS(0)
 print(maxval)
